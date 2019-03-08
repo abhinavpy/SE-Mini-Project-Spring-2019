7bb0265f901759adbb0845dd60aba849527b46ff
abhinavpy <py.abhinav2@gmail.com>
2019-03-08 14:58:31 +0530

    Results of the matrix multiplication posted

--- go_matrix_mult/matrix_mult.go
+++ go_matrix_mult/matrix_mult.go
@@ -1,14 +1,17 @@
 package main
 
 import "fmt"
+import "sync"
+import "time"
+
 
 func main(){
 
-    matrix_a := make([][]int,3);
+    matrix_a := make([][]int,1000);
 
     for i:=0;i<len(matrix_a);i++{
 
-        matrix_a[i]=make([]int,3);
+        matrix_a[i]=make([]int,1000);
 
     }
 
@@ -16,15 +19,15 @@ func main(){
     for i:=0;i<len(matrix_a);i++{
 
         for j:=0;j<len(matrix_a[0]);j++{
-            matrix_a[i][j] = 2;
+            matrix_a[i][j] = 1;
         }
     }
 
-    matrix_b := make([][]int,3);
+    matrix_b := make([][]int,1000);
 
     for i:=0;i<len(matrix_b);i++{
 
-        matrix_b[i]=make([]int,3);
+        matrix_b[i]=make([]int,1000);
 
     }
 
@@ -32,12 +35,11 @@ func main(){
     for i:=0;i<len(matrix_b);i++{
 
         for j:=0;j<len(matrix_b[0]);j++{
-            matrix_b[i][j] = 2;
+            matrix_b[i][j] = 1;
         }
     }
 
     defer fmt.Println(multiply(matrix_a,matrix_b));
-
 }
 
 func pmultiply(matrix_a [][] int,matrix_b [][] int,row int,col int) int{
@@ -52,15 +54,19 @@ func pmultiply(matrix_a [][] int,matrix_b [][] int,row int,col int) int{
 
 func multiply(matrix_a [][] int,matrix_b [][] int) ([][] int){
 
-    matrix_c := make([][]int,3);
+	start := time.Now()
+
+    matrix_c := make([][]int,1000);
 
     for i:=0;i<len(matrix_c);i++{
-        matrix_c[i]=make([]int,3);
+        matrix_c[i]=make([]int,1000);
     }
 
+    
+
     var wg sync.WaitGroup
-	for i := 0; i < 3; i++ {
-    	for j := 0; j < 3; j++ {
+	for i := 0; i < 1000; i++ {
+    	for j := 0; j < 1000; j++ {
 	        wg.Add(1) // Tell the WaitGroup to wait for another thing.
 	        go func(i, j int) {
 	            matrix_c[i][j] = pmultiply(matrix_a, matrix_b, i, j)
@@ -68,7 +74,15 @@ func multiply(matrix_a [][] int,matrix_b [][] int) ([][] int){
 	        }(i, j)
     	}
 	}
-	
+
+
 	wg.Wait()	
+
+	stop := time.Now()
+
+	elapsed := stop.Sub(start)
+
+	fmt.Println(elapsed)
+
     return matrix_c;
 }
